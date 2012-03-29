fbaeb730e06ee29eee136a0c1ef7a2eb18aa4d38
Fred Wenzel <fwenzel@mozilla.com>
2012-03-29 14:30:53 -0700

    dotlang file support. djangoy, this will not work yet.

---
+++ vendor/dotlang/helpers.py
@@ -0,0 +1,11 @@
+import jingo
+import jinja2
+
+from dotlang.translate import translate
+
+
+@jingo.register.function
+@jinja2.contextfunction
+def ___(ctx, text):
+    locale = ctx['request'].locale
+    return jinja2.Markup(translate(locale, text))
